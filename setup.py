101fa82b5ac27f42e027899dd8e4057c9e06a5b1
j-i-k-o <jiko.rogy@gmail.com>
2020-04-17 00:46:52 +0900

    update version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ NAME = 'jij_cimod'
 DESCRIPTION = 'C++ library for a binary quadratic model'
 EMAIL = 'openjij@j-ij.com'
 AUTHOR = 'Jij Inc.'
-VERSION = '1.0.0'
+VERSION = '1.0.1'
 
 
 class CMakeExtension(Extension):
