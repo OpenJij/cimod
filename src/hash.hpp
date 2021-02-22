b3dd48005233d21fcc433a877441bbe562341221
KSuzuki <k.suzuki@j-ij.com>
2021-02-22 21:00:30 +0900

    change "unsigned int" to "size_t"  for the return value of pair_hash to silence implicit conversion warning

--- src/hash.hpp
+++ src/hash.hpp
@@ -81,8 +81,8 @@ namespace cimod
  */
 struct pair_hash {
     template <class T1, class T2>
-    unsigned int operator() (const std::pair<T1, T2>& p) const {
-        unsigned int lhs = std::hash<T1>()(p.first), rhs = std::hash<T2>()(p.second);
+   std::size_t operator() (const std::pair<T1, T2>& p) const {
+        std::size_t lhs = std::hash<T1>()(p.first), rhs = std::hash<T2>()(p.second);
         return lhs^(rhs+0x9e3779b9+(lhs<<6)+(lhs>>2));
     }
 };
