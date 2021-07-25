593750529554669c9f33058731a8adf53f98a5fa
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2021-07-26 07:33:07 +0900

    Update __init__.py

--- cimod/__init__.py
+++ cimod/__init__.py
@@ -1,5 +1,8 @@
-
-from .__version import __version__
-from .vartype import SPIN, BINARY, Vartype
-from .model.binary_quadratic_model import make_BinaryQuadraticModel, make_BinaryQuadraticModel_from_JSON, BinaryQuadraticModel
-from .model.binary_polynomial_model import make_BinaryPolynomialModel, make_BinaryPolynomialModel_from_JSON, BinaryPolynomialModel
+try:
+    import typing 
+except ImportError:
+    from typing_extensions import *
+from cimod.__version import __version__
+from cimod.vartype import SPIN, BINARY, Vartype
+from cimod.model.binary_quadratic_model import make_BinaryQuadraticModel, make_BinaryQuadraticModel_from_JSON, BinaryQuadraticModel
+from cimod.model.binary_polynomial_model import make_BinaryPolynomialModel, make_BinaryPolynomialModel_from_JSON, BinaryPolynomialModel
