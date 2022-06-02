87d03ae9a5ea9c7f8145517e2c69a40a7f439979
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2022-06-03 07:56:50 +0900

    Update __init__.py

--- cimod/model/__init__.py
+++ cimod/model/__init__.py
@@ -2,5 +2,7 @@ from pkgutil import extend_path
 
 __path__ = extend_path(__path__, __name__)
 
-from .binary_quadratic_model import make_BinaryQuadraticModel, make_BinaryQuadraticModel_from_JSON, BinaryQuadraticModel
-from .binary_polynomial_model import make_BinaryPolynomialModel, make_BinaryPolynomialModel_from_JSON, BinaryPolynomialModel
+import cimod.model.legacy
+
+from cimod.model.binary_quadratic_model import make_BinaryQuadraticModel, make_BinaryQuadraticModel_from_JSON, BinaryQuadraticModel
+from cimod.model.binary_polynomial_model import make_BinaryPolynomialModel, make_BinaryPolynomialModel_from_JSON, BinaryPolynomialModel
