db70ead2c0836a237937d48de041e82d6e2dc558
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2022-06-02 16:00:16 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,16 @@ import sys
 from packaging.version import LegacyVersion
 from skbuild.exceptions import SKBuildError
 from skbuild.cmaker import get_cmake_version
-from skbuild import setup
+
+try:
+    from skbuild import setup
+except ImportError:
+    print(
+        "Please update pip, you need pip 10 or greater,\n"
+        " or you need to install the PEP 518 requirements in pyproject.toml yourself",
+        file=sys.stderr,
+    )
+    raise
 
 setup_requires=[ 
   'pybind11', 
@@ -28,7 +37,7 @@ setup(
 		'cimod.model.legacy',
 		'cimod.utils',
         ],
-	package_dir={'': 'cimod'},
-        #cmake_install_dir='cimod',
+        cmake_install_dir='cimod',
+	include_package_data=True,
 	zip_safe=False,
 )
