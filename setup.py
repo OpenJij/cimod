c6bdea7ad7341a08ba6e84d105ae06d92af29509
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2022-06-02 13:23:56 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from skbuild import setup
 
 setup(
-    cmake_install_dir='cimod'
+    cmake_source_dir = "cimod/", 
+    cmake_with_sdist = True, 
+    cmake_languages = ('C', 'CXX',), 
+    cmake_minimum_required_version = '2.20', 
+    
 )
