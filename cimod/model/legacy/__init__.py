bde9a505f935e65fe39630c85bda7c655b423afa
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2022-06-03 07:59:20 +0900

    Update __init__.py

--- cimod/model/legacy/__init__.py
+++ cimod/model/legacy/__init__.py
@@ -2,4 +2,6 @@ from pkgutil import extend_path
 
 __path__ = extend_path(__path__, __name__)
 
-from .binary_quadratic_model import BinaryQuadraticModel
+from cimod.model.legacy.binary_quadratic_model import BinaryQuadraticModel
+
+__all__ = ["BinaryQuadraticModel"]
