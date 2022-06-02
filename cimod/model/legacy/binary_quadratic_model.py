98e2e6a3bab238ae550b318c7218696c6958ce2a
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2022-06-03 08:04:48 +0900

    Update binary_quadratic_model.py

--- cimod/model/legacy/binary_quadratic_model.py
+++ cimod/model/legacy/binary_quadratic_model.py
@@ -1,4 +1,4 @@
-# Copyright 2021 Jij Inc.
+# Copyright 2022 Jij Inc.
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,6 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+from __future__ import annotations
 
 import cimod.cxxcimod as cxxcimod
 import cimod
