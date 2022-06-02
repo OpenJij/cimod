d00cbb36a434da5caf11e574c8bf2a77a1f01850
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2022-06-03 08:04:22 +0900

    Update binary_quadratic_model.py

--- cimod/model/binary_quadratic_model.py
+++ cimod/model/binary_quadratic_model.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from __future__ import annotations
+
 import cimod.cxxcimod as cxxcimod
 import cimod
 from cimod.vartype import to_cxxcimod
