0fef0913e90687d8b437edf32848f8bd45b0b1e6
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2022-06-03 07:58:21 +0900

    Update __init__.py

--- cimod/model/__init__.py
+++ cimod/model/__init__.py
@@ -6,3 +6,12 @@ import cimod.model.legacy
 
 from cimod.model.binary_quadratic_model import make_BinaryQuadraticModel, make_BinaryQuadraticModel_from_JSON, BinaryQuadraticModel
 from cimod.model.binary_polynomial_model import make_BinaryPolynomialModel, make_BinaryPolynomialModel_from_JSON, BinaryPolynomialModel
+
+__all__ = [ 
+  "make_BinaryQuadraticModel", 
+  "make_BinaryQuadraticModel_from_JSON", 
+  "BinaryQuadraticModel", 
+  "make_BinaryPolynomialModel", 
+  "make_BinaryPolynomialModel_from_JSON", 
+  "BinaryPolynomialModel",
+]
