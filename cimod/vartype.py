454a7dff48c832dd53199cbf434f567bbcb86a5f
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2022-06-02 16:16:12 +0900

    Update vartype.py

--- cimod/vartype.py
+++ cimod/vartype.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import cxxcimod
+import cimod.cxxcimod
 import dimod
 
 SPIN = dimod.SPIN
