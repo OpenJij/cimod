1f0e7d3cde995c6fee84d4d8e7420c046cd1066b
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2022-06-02 18:12:51 +0900

    Update __init__.py

--- cimod/__init__.py
+++ cimod/__init__.py
@@ -6,10 +6,22 @@ try:
     import typing 
 except ImportError:
     from typing_extensions import * 
-import cimod.cxxcimod 
-import cimod.utils
-import cimod.model 
-import cimod.model.legacy
+#import cimod.cxxcimod 
+#import cimod.utils
+#import cimod.model 
+#import cimod.model.legacy
 from cimod.vartype import SPIN, BINARY, Vartype
 from cimod.model.binary_quadratic_model import make_BinaryQuadraticModel, make_BinaryQuadraticModel_from_JSON, BinaryQuadraticModel
 from cimod.model.binary_polynomial_model import make_BinaryPolynomialModel, make_BinaryPolynomialModel_from_JSON, BinaryPolynomialModel
+
+__all__ = [ 
+    "SPIN", 
+    "BINARY", 
+    "Vartype", 
+    "make_BinaryQuadraticModel", 
+    "make_BinaryQuadraticModel_from_JSON", 
+    "BinaryQuadraticModel", 
+    "make_BinaryPolynomialModel", 
+    "make_BinaryPolynomialModel_from_JSON", 
+    "BinaryPolynomialModel",
+]
