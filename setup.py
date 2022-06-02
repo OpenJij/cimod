bb66d5df1a094c1aa403d11755c3cf82dbe23e2c
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2022-06-02 14:21:25 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,20 @@
+from skbuild.cmaker import CMaker
 from skbuild import setup
 
-setup()
+setup(
+  setup_requires = [ 
+    "setuptools_scm[toml]",
+    "pytest-runner",
+    "cmake",
+    "pybind11",
+    "scikit-build",
+    "setuptools",
+  ],
+  packages = [ 
+    "cimod", 
+    "cimod.model", 
+    "cimod.model.legacy", 
+    "cimod.utils",
+  ],
+  zip_safe = False,
+)
