7468437950b2f9ebd084b36c33f61a056d92ada2
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2022-06-02 14:48:33 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,39 @@
-from skbuild.cmaker import CMaker
+import sys
+
+from packaging.version import LegacyVersion
+from skbuild.exceptions import SKBuildError
+from skbuild.cmaker import get_cmake_version
 from skbuild import setup
 
+setup_requires=[ 
+  'pybind11', 
+  'pytest-runner',
+]
+
+# Require pytest-runner only when running tests.
+if any(arg in sys.argv for arg in ('pytest', 'test')):
+	setup_requires.append('pytest-runner')
+
+# Add CMake as a build requirement if cmake is not installed or is too low a version.
+try:
+    if LegacyVersion(get_cmake_version()) < LegacyVersion
+        setup_requires.append('cmake')
+except SKBuildError:
+    setup_requires.append('cmake')  
+
 setup(
-  setup_requires = [ 
-    "setuptools_scm[toml]",
-    "pytest-runner",
-    "cmake",
-    "pybind11",
-    "scikit-build",
-    "setuptools",
-  ],
-  packages = [ 
-    "cimod", 
-    "cimod.model", 
-    "cimod.model.legacy", 
-    "cimod.utils",
-  ],
-  zip_safe = False,
+    setup_requires=[ 
+      'cmake', 
+      'pybind11', 
+      'scikit-build', 
+      'pytest-runner',
+    ]
+    packages=[
+      'cimod',
+      'cimod.model',
+      'cimod.model.legacy', 
+      'cimod.utils',
+    ],
+    package_dir={'': 'cimod'},
+    zip_safe=False,
 )
