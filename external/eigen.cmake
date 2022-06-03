5c263059216399b135699cd6b1cb69503533d9bc
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2022-06-03 16:56:45 +0900

    Update eigen.cmake

--- external/eigen.cmake
+++ external/eigen.cmake
@@ -9,21 +9,27 @@ FetchContent_Declare(
     )
     
 set(EIGEN_MPL2_ONLY ON)
-FetchContent_MakeAvailable(eigen)
 
-add_library(cimod-eigen_lib INTERFACE)
-set_target_properties(cimod-eigen_lib PROPERTIES
-    EIGEN_MPL2_ONLY ON
-)
-target_include_directories(cimod-eigen_lib INTERFACE ${eigen_SOURCE_DIR})
-target_compile_definitions(cimod-eigen_lib INTERFACE EIGEN_MPL2_ONLY)
+set_target_properties(eigen PROPERTIES
+     EIGEN_MPL2_ONLY ON
+     BUILD_TESTING OFF
+) 
+
+#add_library(cimod-eigen_lib INTERFACE)
+#set_target_properties(cimod-eigen_lib PROPERTIES
+#    EIGEN_MPL2_ONLY ON
+#)
+#target_include_directories(cimod-eigen_lib INTERFACE ${eigen_SOURCE_DIR})
+#target_compile_definitions(cimod-eigen_lib INTERFACE EIGEN_MPL2_ONLY)
 if (APPLE)
     if(BLAS_FOUND AND LAPACK_FOUND) 
-      target_compile_definitions(cimod-eigen_lib INTERFACE EIGEN_USE_BLAS=ON)
-      target_compile_definitions(cimod-eigen_lib INTERFACE EIGEN_USE_LAPACKE=ON)
+      target_compile_definitions(eigen INTERFACE EIGEN_USE_BLAS=ON)
+      target_compile_definitions(eigen INTERFACE EIGEN_USE_LAPACKE=ON)
     endif()
 endif()
 
 if(OpenMP_FOUND)
-  target_link_libraries(cimod-eigen_lib INTERFACE OpenMP::OpenMP_CXX)
+  target_link_libraries(eigen INTERFACE OpenMP::OpenMP_CXX)
 endif()
+
+FetchContent_MakeAvailable(eigen)
