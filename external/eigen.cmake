541dadba880a3f473b09900589deacfc6b55a273
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2022-06-03 16:14:17 +0900

    Update eigen.cmake

--- external/eigen.cmake
+++ external/eigen.cmake
@@ -4,23 +4,15 @@ include(FetchContent)
 FetchContent_Declare(
     eigen
     GIT_REPOSITORY  https://gitlab.com/libeigen/eigen
-    GIT_TAG         3.3.9
-    CMAKE_ARGS -DEIGEN_MPL2_ONLY
+    GIT_TAG          3.4.0
+    CMAKE_ARGS -DEIGEN_MPL2_ONLY=ON -DBUILD_TESTING=OFF
     )
     
-set(EIGEN_BUILD_DOC OFF)
-set(BUILD_TESTING OFF)
-set(EIGEN_BUILD_PKGCONFIG OFF)
-set(EIGEN_CPP_STANDARD 11)
 set(EIGEN_MPL2_ONLY ON)
 FetchContent_MakeAvailable(eigen)
 
 add_library(cimod-eigen_lib INTERFACE)
 set_target_properties(cimod-eigen_lib PROPERTIES
-    EIGEN_BUILD_DOC OFF
-    BUILD_TESTING OFF
-    EIGEN_BUILD_PKGCONFIG OFF
-    EIGEN_CPP_STANDARD 11
     EIGEN_MPL2_ONLY ON
 )
 target_include_directories(cimod-eigen_lib INTERFACE ${eigen_SOURCE_DIR})
