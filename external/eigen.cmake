f064943a59fe1a790dad88067191307ca8fdff54
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2022-06-03 17:57:53 +0900

    Update eigen.cmake

--- external/eigen.cmake
+++ external/eigen.cmake
@@ -2,16 +2,21 @@ include(FetchContent)
 
 #### Eigen ####
 FetchContent_Declare(
-    eigen
+    Eigen
     GIT_REPOSITORY  https://gitlab.com/libeigen/eigen
     GIT_TAG          3.4.0
-    CMAKE_ARGS -DEIGEN_MPL2_ONLY=ON -DBUILD_TESTING=OFF
-    )
-    
+    GIT_SHALLOW TRUE
+    GIT_PROGRESS TRUE
+)
+ 
 set(EIGEN_MPL2_ONLY ON)
 set(BUILD_TESTING OFF) 
-set(EIGEN_BUILD_TESTING OFF) 
-set(EIGEN_BUILD_TEST OFF)
+set(EIGEN_LEAVE_TEST_IN_ALL_TARGET OFF) 
+set(EIGEN_BUILD_PKGCONFIG OFF) 
+set(EIGEN_TEST_NOQT OFF)
+
+FetchContent_MakeAvailable(Eigen)
+
 #set_target_properties(eigen PROPERTIES
 #     EIGEN_MPL2_ONLY ON
 #     BUILD_TESTING OFF
@@ -19,18 +24,24 @@ set(EIGEN_BUILD_TEST OFF)
 #     EIGEN_BUILD_TEST OFF
 #) 
 
-#add_library(cimod-eigen_lib INTERFACE)
-#set_target_properties(cimod-eigen_lib PROPERTIES
-#    EIGEN_MPL2_ONLY ON
-#)
-#target_include_directories(cimod-eigen_lib INTERFACE ${eigen_SOURCE_DIR})
+add_library(cimod-eigen_lib INTERFACE)
+set_target_properties(cimod-eigen_lib PROPERTIES
+    EIGEN_MPL2_ONLY ON
+    BUILD_TESTING OFF
+    EIGEN_LEAVE_TEST_IN_ALL_TARGET OFF
+    EIGEN_BUILD_PKGCONFIG OFF
+    EIGEN_TEST_NOQT OFF
+)
+target_include_directories(cimod-eigen_lib INTERFACE ${eigen_SOURCE_DIR})
 #target_compile_definitions(cimod-eigen_lib INTERFACE EIGEN_MPL2_ONLY)
 if (APPLE)
     if(BLAS_FOUND AND LAPACK_FOUND) 
-      set(EIGEN_USE_BLAS ON)
-      set(EIGEN_USE_LAPACKE ON)
+      set_target_properties(cimod-eigen_lib PROPERTIES
+        EIGEN_USE_BLAS ON
+        EIGEN_USE_LAPACKE O
+      )
     endif()
 endif()
 
 
-FetchContent_MakeAvailable(eigen)
+
