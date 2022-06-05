665d84bdbc941084f095f8d49dfb14d4e1cde55e
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2022-06-06 08:10:36 +0900

    Update eigen.cmake

--- external/eigen.cmake
+++ external/eigen.cmake
@@ -2,7 +2,7 @@ include(FetchContent)
 
 #### Eigen ####
 FetchContent_Declare(
-    eigen
+    Eigen
     GIT_REPOSITORY  https://gitlab.com/libeigen/eigen
     GIT_TAG          3.4.0
     GIT_SHALLOW TRUE
@@ -19,14 +19,7 @@ set(EIGEN_BUILD_DOC OFF)
 set(EIGEN_TEST_NOQT OFF)
 set(EIGEN_SPLIT_LARGE_TESTS OFF)
 set(EIGEN_NO_ASSERTION_CHECKING OFF)
-
-FetchContent_MakeAvailable(eigen)
-FetchContent_GetProperties(eigen)
-
-find_package(eigen3)
-find_package(Eigen3)
-
-message(STATUS "eigen_SOURCE_DIR = ${eigen_SOURCE_DIR}")
+set( OFF)
 
 #set_target_properties(eigen PROPERTIES
 #     EIGEN_MPL2_ONLY ON
@@ -35,28 +28,33 @@ message(STATUS "eigen_SOURCE_DIR = ${eigen_SOURCE_DIR}")
 #     EIGEN_BUILD_TEST OFF
 #) 
 
-add_library(cimod-eigen_lib INTERFACE)
-set_target_properties(cimod-eigen_lib PROPERTIES
-    EIGEN_MPL2_ONLY ON
-    BUILD_TESTING OFF
-    EIGEN_LEAVE_TEST_IN_ALL_TARGET OFF
-    EIGEN_SPLIT_LARGE_TESTS OFF
-    EIGEN_BUILD_PKGCONFIG OFF
-    EIGEN_BUILD_DOC OFF
-    EIGEN_TEST_NOQT OFF
-    EIGEN_BUILD_BTL OFF
-    EIGEN_NO_ASSERTION_CHECKING OFF
-)
-target_include_directories(cimod-eigen_lib INTERFACE ${eigen_SOURCE_DIR})
+#add_library(cimod-eigen_lib INTERFACE)
+#set_target_properties(cimod-eigen_lib PROPERTIES
+#    EIGEN_MPL2_ONLY ON
+#    BUILD_TESTING OFF
+#    EIGEN_LEAVE_TEST_IN_ALL_TARGET OFF
+#    EIGEN_SPLIT_LARGE_TESTS OFF
+#    EIGEN_BUILD_PKGCONFIG OFF
+#    EIGEN_BUILD_DOC OFF
+#    EIGEN_TEST_NOQT OFF
+#    EIGEN_BUILD_BTL OFF
+#    EIGEN_NO_ASSERTION_CHECKING OFF
+#)
+#target_include_directories(cimod-eigen_lib INTERFACE ${eigen_SOURCE_DIR})
 #target_compile_definitions(cimod-eigen_lib INTERFACE EIGEN_MPL2_ONLY)
 if (APPLE)
     if(BLAS_FOUND AND LAPACK_FOUND) 
-      set_target_properties(cimod-eigen_lib PROPERTIES
-        EIGEN_USE_BLAS ON
-        EIGEN_USE_LAPACKE O
-      )
+      set(EIGEN_USE_BLAS ON)
+      set(EIGEN_USE_LAPACKE O)
     endif()
 endif()
 
+FetchContent_MakeAvailable(Eigen)
+FetchContent_GetProperties(Eigen)
 
+find_package(eigen3)
+find_package(Eigen3)
 
+message(STATUS "eigen_SOURCE_DIR = ${eigen_SOURCE_DIR}")
+
+target_link_libraries(cxxcimod_header_only PRIVATE Eigen3::Eigen)
