1b18b23ebc10813bf1b8394cf696c1168e1a172a
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2022-06-06 06:44:03 +0900

    Update test_bqm.hpp

--- tests/test_bqm.hpp
+++ tests/test_bqm.hpp
@@ -12,9 +12,9 @@
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
 
-#include "gtest/gtest.h"
+#include <gtest/gtest.h>
 
-#include "../src/binary_quadratic_model.hpp"
+#include <binary_quadratic_model.hpp>
 
 #include <nlohmann/json.hpp>
 
