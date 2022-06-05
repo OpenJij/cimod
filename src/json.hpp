f4a5998c971515f751f5c91a9ba01344b2bf09b2
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2022-06-06 07:18:58 +0900

    Update json.hpp

--- src/json.hpp
+++ src/json.hpp
@@ -12,4 +12,4 @@
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
 
-#include "../external/nlohmann_json/nlohmann/json.hpp"
+#include <nlohmann/json.hpp>
