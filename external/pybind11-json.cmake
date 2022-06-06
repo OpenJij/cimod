39f6853d5d04d3422eca96f1160e4c16870e06e7
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2022-06-06 12:04:59 +0900

    Update pybind11-json.cmake

--- external/pybind11-json.cmake
+++ external/pybind11-json.cmake
@@ -1,5 +1,9 @@
 include(FetchContent)
 
+message(CHECK_START "Fetching Pybind11 Json")
+list(APPEND CMAKE_MESSAGE_INDENT "  ")
+
+set(FETCHCONTENT_QUIET OFF)
 #### pybind11_json ####
 FetchContent_Declare(
     pybind11_json
@@ -9,3 +13,6 @@ FetchContent_Declare(
 )
 set(BUILD_TESTING OFF)
 FetchContent_MakeAvailable(pybind11_json)
+
+list(POP_BACK CMAKE_MESSAGE_INDENT)
+message(CHECK_PASS "fetched")
