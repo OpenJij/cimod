0d53e29bb111a3a2ecef105bdff39c13371d6dca
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2022-06-06 16:49:02 +0900

    Update eigen.cmake

--- external/eigen.cmake
+++ external/eigen.cmake
@@ -5,7 +5,8 @@ list(APPEND CMAKE_MESSAGE_INDENT "  ")
 
 set(FETCHCONTENT_QUIET OFF)
 
-set(BUILD_TESTING OFF)
+set(BUILD_TESTING OFF) 
+set(EIGEN_MPL2_ONLY ON)
 set(EIGEN_BUILD_PKGCONFIG OFF)
 set(EIGEN_BUILD_DOC OFF) 
 set(EIGEN_BUILD_TESTING OFF)
@@ -16,18 +17,25 @@ FetchContent_Declare(
     eigen
     GIT_REPOSITORY  https://gitlab.com/libeigen/eigen
     GIT_TAG         3.4.0
-    CMAKE_ARGS -DEIGEN_MPL2_ONLY
+    CMAKE_ARGS 
+    -DEIGEN_MPL2_ONLY=ON 
+    -DEIGEN_BUILD_PKGCONFIG=OFF
+    -DEIGEN_BUILD_DOC=OFF
+    -DEIGEN_BUILD_TESTING=OFF
+    -DEIGEN_Fortran_COMPILER_WORKS=OFF
     )
 
-set(EIGEN_MPL2_ONLY ON)
 FetchContent_MakeAvailable(eigen)
 
 add_library(cimod-eigen_lib INTERFACE)
 target_include_directories(cimod-eigen_lib INTERFACE ${eigen_SOURCE_DIR})
 target_compile_definitions(cimod-eigen_lib INTERFACE EIGEN_MPL2_ONLY)
 if(BLAS_FOUND AND LAPACK_FOUND) 
-    set(EIGEN_USE_BLAS ON)
-    set(EIGEN_USE_LAPACKE ON)
+    target_compile_definitions(cimod-eigen_lib INTERFACE EIGEN_USE_BLAS EIGEN_USE_LAPACKE)
+    set_target_properties(cimod-eigen_lib INTERFACE
+      EIGEN_USE_BLAS ON
+      EIGEN_USE_LAPACKE ON
+    )
 endif()
 
 list(POP_BACK CMAKE_MESSAGE_INDENT)
