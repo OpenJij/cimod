4a3e1bcae44abe6778e1d6477e06d091e90b7d7a
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2022-06-06 16:35:30 +0900

    Update eigen.cmake

--- external/eigen.cmake
+++ external/eigen.cmake
@@ -1,53 +1,33 @@
-include(ExternalProject)
+include(FetchContent)
 
 message(CHECK_START "Fetching Eigen3")
 list(APPEND CMAKE_MESSAGE_INDENT "  ")
 
-set(BUILD_TESTING OFF)
+set(FETCHCONTENT_QUIET OFF)
 
+set(BUILD_TESTING OFF)
+set(EIGEN_BUILD_PKGCONFIG OFF)
+set(EIGEN_BUILD_DOC OFF) 
+set(EIGEN_BUILD_TESTING OFF)
 set(EIGEN_Fortran_COMPILER_WORKS OFF)
-set(EIGEN_INSTALL_DIR "${CMAKE_CURRENT_SOURCE_DIR}/eigen-install/")
 
 #### Eigen ####
-
-ExternalProject_Add(
+FetchContent_Declare(
     eigen
-    URL  https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.tar.gz
-    SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/eigen-src"
-    BINARY_DIR "${CMAKE_CURRENT_SOURCE_DIR}/eigen-build"
-    INSTALL_DIR "${EIGEN_INSTALL_DIR}"
-    CMAKE_ARGS
-    -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
-    -DCMAKE_BUILD_TYPE=Release
-    -DEIGEN_MPL2_ONLY=ON
-    -DEIGEN_BUILD_PKGCONFIG=OFF 
-    -DEIGEN_BUILD_DOC=OFF 
-    -DEIGEN_BUILD_TESTING=OFF
-    -DEIGEN_Fortran_COMPILER_WORKS=OFF
-)
-
-file(MAKE_DIRECTORY ${EIGEN_INSTALL_DIR}/include)  # avoid race condition
-
-add_library(eigenlib INTERFACE IMPORTED GLOBAL)
-add_dependencies(eigenlib eigen)
-target_compile_features(eigenlib INTERFACE cxx_std_14)
-
-set_target_properties(eigenlib PROPERTIES
-  INTERFACE_INCLUDE_DIRECTORIES ${EIGEN_INSTALL_DIR}/include/eigen3
-  EIGEN_MPL2_ONLY ON
-  EIGEN_BUILD_PKGCONFIG OFF 
-  EIGEN_BUILD_DOC OFF 
-  EIGEN_BUILD_TESTING OFF
-  EIGEN_Fortran_COMPILER_WORKS OFF
-)
-
-if(BLAS_FOUND AND LAPACK_FOUND) 
-  if(NOT WIN32)
-    set_target_properties(eigenlib PROPERTIES 
-      EIGEN_USE_BLAS ON
-      EIGEN_USE_LAPACKE ON
+    GIT_REPOSITORY  https://gitlab.com/libeigen/eigen
+    GIT_TAG         3.4.0
+    CMAKE_ARGS -DEIGEN_MPL2_ONLY
     )
-  endif()
+
+set(EIGEN_MPL2_ONLY ON)
+FetchContent_MakeAvailable(eigen)
+
+add_library(cimod-eigen_lib INTERFACE)
+target_include_directories(cimod-eigen_lib INTERFACE ${eigen_SOURCE_DIR})
+target_compile_definitions(cimod-eigen_lib INTERFACE EIGEN_MPL2_ONLY)
+if(BLAS_FOUND AND LAPACK_FOUND) 
+    set(INTERFACE EIGEN_USE_BLAS ON)
+    set(EIGEN_USE_LAPACKE ON)
 endif()
 
 list(POP_BACK CMAKE_MESSAGE_INDENT)
