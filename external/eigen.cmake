6a012f84d4658f6852b0c47ec15b46692fa2f45b
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2022-06-06 15:17:45 +0900

    Update eigen.cmake

--- external/eigen.cmake
+++ external/eigen.cmake
@@ -1,43 +1,49 @@
-include(FetchContent)
+include(ExternalProject)
 
 message(CHECK_START "Fetching Eigen3")
 list(APPEND CMAKE_MESSAGE_INDENT "  ")
 
-set(FETCHCONTENT_QUIET OFF)
-
-#### Eigen ####
-FetchContent_Declare(
-    Eigen
-    GIT_REPOSITORY  https://gitlab.com/libeigen/eigen
-    GIT_TAG          3.4.0
-    GIT_SHALLOW TRUE
-)
-
 set(BUILD_TESTING OFF)
 
-set(EIGEN_BUILD_PKGCONFIG OFF)
-set(EIGEN_BUILD_DOC OFF)
-set(EIGEN_BUILD_TESTING OFF)
+set(EIGEN_INSTALL_DIR "${CMAKE_CURRENT_SOURCE_DIR}/eigen-install/")
 
-set(EIGEN_MPL2_ONLY ON)
+#### Eigen ####
 
-set( OFF)
+ExternalProject_Add(
+    eigen
+    URL  https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.tar.gz
+    SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/eigen-src"
+    BINARY_DIR "${CMAKE_CURRENT_SOURCE_DIR}/eigen-build"
+    INSTALL_DIR "${EIGEN_INSTALL_DIR}"
+    CMAKE_ARGS
+    -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
+    -DCMAKE_BUILD_TYPE=Release
+    -DEIGEN_MPL2_ONLY=ON
+    -DEIGEN_BUILD_PKGCONFIG=OFF 
+    -DEIGEN_BUILD_DOC=OFF 
+    -DEIGEN_BUILD_TESTING=OFF
+)
+
+file(MAKE_DIRECTORY ${EIGEN_INSTALL_DIR}/include)  # avoid race condition
+
+add_library(eigenlib INTERFACE IMPORTED GLOBAL)
+add_dependencies(eigenlib eigen)
+target_compile_features(eigenlib INTERFACE cxx_std_14)
+
+set_target_properties(eigenlib PROPERTIES
+  INTERFACE_INCLUDE_DIRECTORIES ${EIGEN_INSTALL_DIR}/include/eigen3
+  EIGEN_MPL2_ONLY ON
+  EIGEN_BUILD_PKGCONFIG OFF 
+  EIGEN_BUILD_DOC OFF 
+  EIGEN_BUILD_TESTING OFF
+)
 
 if(BLAS_FOUND AND LAPACK_FOUND) 
-  set(EIGEN_USE_BLAS ON)
-  set(EIGEN_USE_LAPACKE ON)
+  set_target_properties(eigenlib PROPERTIES 
+    EIGEN_USE_BLAS ON
+    EIGEN_USE_LAPACKE ON
+  )
 endif()
-
-FetchContent_MakeAvailable(Eigen)
-#FetchContent_GetProperties(Eigen)
-
-#find_package(Eigen3)
-
-#if(Eigen3_FOUND)
-#    message(STATUS "Found Eigen3")
-#endif()
-
-message(STATUS "eigen_SOURCE_DIR = ${eigen_SOURCE_DIR}")
 
 list(POP_BACK CMAKE_MESSAGE_INDENT)
 message(CHECK_PASS "fetched")
