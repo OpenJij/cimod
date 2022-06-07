9fb20b4557fedb8e57e144b4c624a961514765b7
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2022-06-07 16:07:28 +0900

    Update googletest.cmake

--- external/googletest.cmake
+++ external/googletest.cmake
@@ -9,7 +9,7 @@ set(FETCHCONTENT_QUIET OFF)
 #### Google test ####
 FetchContent_Declare(
     googletest
-    GIT_REPOSITORY  https://github.com/google/googletest.git
+    GIT_REPOSITORY  https://github.com/google/googletest
     GIT_TAG         release-1.11.0
     GIT_SHALLOW     TRUE
 )
