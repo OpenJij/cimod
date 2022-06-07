4046821199068921e80cf73832bc281ccf09cb2b
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2022-06-07 22:50:14 +0900

    Update eigen.cmake

--- external/eigen.cmake
+++ external/eigen.cmake
@@ -35,12 +35,6 @@ FetchContent_Declare(
     GIT_REPOSITORY  https://gitlab.com/libeigen/eigen
     GIT_TAG         3.4.0
     GIT_SHALLOW     TRUE
-    CMAKE_ARGS 
-    -DEIGEN_MPL2_ONLY=ON 
-    -DEIGEN_BUILD_PKGCONFIG=OFF
-    -DEIGEN_BUILD_DOC=OFF
-    -DEIGEN_BUILD_TESTING=OFF
-    -DEIGEN_Fortran_COMPILER_WORKS=OFF
     )
 
 FetchContent_MakeAvailable(eigen)
@@ -48,6 +42,7 @@ FetchContent_MakeAvailable(eigen)
 add_library(cimod-eigen_lib INTERFACE)
 target_include_directories(cimod-eigen_lib INTERFACE ${eigen_SOURCE_DIR})
 target_compile_definitions(cimod-eigen_lib INTERFACE EIGEN_MPL2_ONLY)
+set_target_properties(cimod-eigen_lib INTERFACE EIGEN_MPL2_ONLY)
 
 list(POP_BACK CMAKE_MESSAGE_INDENT)
 message(CHECK_PASS "fetched")
