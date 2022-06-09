824b089d2859a4f3e6dfe7cdc66b34d9b2478758
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2022-06-10 07:51:49 +0900

    Update main.cpp

--- cimod/main.cpp
+++ cimod/main.cpp
@@ -12,7 +12,7 @@
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
 
-#include "impl.hpp"
+
 #include <string>
 
 namespace py = pybind11;
