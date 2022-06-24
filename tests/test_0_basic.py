eabd0949e0dae93bffc03c100e6c3619f9b2e6b1
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2022-06-24 14:33:10 +0900

    Update test_0_basic.py

--- tests/test_0_basic.py
+++ tests/test_0_basic.py
@@ -34,6 +34,5 @@ def test_no_self_loop_allowed():
   import cimod
   with pytest.raises(RuntimeError) as exception:
      cimod.BinaryQuadraticModel({}, {(2,2):5}, "SPIN")
-     pprint(traceback.format_exc(), compact=True)
-  pprint(traceback.format_exc(), compact=True)
-  pprint.pprint(exception, compact=True)
+     traceback.print_exc()
+  pprint.pprint(exception)
