f128e20e1a33e3eeffee987a90aab24fb7b9bbad
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2022-12-14 11:23:42 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,8 +14,6 @@
 
 import sys
 
-from packaging.version import LegacyVersion
-
 try:
     from skbuild import setup
     from skbuild.cmaker import get_cmake_version
@@ -31,17 +29,12 @@ except ImportError:
 setup_requires = [
     "numpy",
     "pybind11 >=2.10.0, < 2.11.0",
+    "cmake > 3.20",
+    "scikit-build > 0.16.0"
 ]
 
 if any(arg in sys.argv for arg in ("pytest", "test")):
     setup_requires.append("pytest-runner")
-
-# Add CMake as a build requirement if cmake is not installed or is too low a version.
-try:
-    if LegacyVersion(get_cmake_version()) < LegacyVersion("3.20"):
-        setup_requires.append("cmake")
-except SKBuildError:
-    setup_requires.append("cmake")
 
 setup(
     setup_requires=setup_requires,
