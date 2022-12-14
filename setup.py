fc9142d9578ab1d17baa1633f780cf7641a3d72f
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2022-12-14 11:11:38 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,34 +14,25 @@
 
 import sys
 
-from packaging.version import LegacyVersion
-
 try:
     from skbuild import setup
-    from skbuild.cmaker import get_cmake_version
-    from skbuild.exceptions import SKBuildError
 except ImportError:
+    from setuptools import setup
     print(
         "Please update pip, you need pip 10 or greater,\n"
         " or you need to install the PEP 518 requirements in pyproject.toml yourself",
         file=sys.stderr,
     )
-    raise
+    
 
 setup_requires = [
     "numpy",
     "pybind11 >=2.10.0, < 2.11.0",
+    "cmake >= 3,20",
 ]
 
 if any(arg in sys.argv for arg in ("pytest", "test")):
     setup_requires.append("pytest-runner")
-
-# Add CMake as a build requirement if cmake is not installed or is too low a version.
-try:
-    if LegacyVersion(get_cmake_version()) < LegacyVersion("3.20"):
-        setup_requires.append("cmake")
-except SKBuildError:
-    setup_requires.append("cmake")
 
 setup(
     setup_requires=setup_requires,
