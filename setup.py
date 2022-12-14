7ae40c71ca26196a8dedd203a3209e4d88cb159f
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2022-12-14 11:26:10 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,15 +16,8 @@ import sys
 
 try:
     from skbuild import setup
-    from skbuild.cmaker import get_cmake_version
-    from skbuild.exceptions import SKBuildError
 except ImportError:
-    print(
-        "Please update pip, you need pip 10 or greater,\n"
-        " or you need to install the PEP 518 requirements in pyproject.toml yourself",
-        file=sys.stderr,
-    )
-    raise
+    from setuptools import setup
 
 setup_requires = [
     "numpy",
